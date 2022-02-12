f04899570b969055b659e501773998c5467c13e8
Shivam Kumar Tiwari <tshivam057@gmail.com>
2022-02-12 21:13:55 +0530

    added message in sum.java

--- addtwonumber.java
+++ addtwonumber.java
@@ -1,8 +1,11 @@
+import javax.sound.sampled.SourceDataLine;
+
 public class addtwonumber {
     public static void main(String[] args) {
         int x= 5;
         int y =6;
         int multi = x*y;
+        System.out.println(" the multiple is ");
         System.out.println(multi);
     }
 }
