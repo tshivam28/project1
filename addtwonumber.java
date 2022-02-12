534ad594f48f70a1a090f1a546924ca77d658092
Shivam Kumar Tiwari <tshivam057@gmail.com>
2022-02-12 21:04:51 +0530

    initail commit

---
+++ addtwonumber.java
@@ -0,0 +1,8 @@
+public class addtwonumber {
+    public static void main(String[] args) {
+        int x= 5;
+        int y =6;
+        int multi = x*y;
+        System.out.println(multi);
+    }
+}
